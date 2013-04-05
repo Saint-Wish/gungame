474e6d36b9c7b1d8819247b4b355e70b1a6b9014
Seamus Fuller <s.fuller468@gmail.com>
2013-04-05 19:01:15 -0400

    Tiny little insignificant patch
    Took out some, uh, debug information

--- gungaym/gamemode/cl_deathnotices.lua
+++ gungaym/gamemode/cl_deathnotices.lua
@@ -1,7 +1,5 @@
 
-print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")print("fuck")
 local hud_deathnotice_time = CreateConVar( "hud_deathnotices_time", "6", FCVAR_REPLICATED, "Amount of time to show death notice" )
-print("fuck")
 -- These are our kill icons
 local Color_Icon = Color( 255, 80, 0, 255 ) 
 local NPC_Color = Color( 250, 50, 50, 255 ) 
