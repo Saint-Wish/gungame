9223276c3c34475fff6285c7b7d339ea4d664dac
Seamus Fuller <s.fuller468@gmail.com>
2013-09-29 20:08:42 -0400

    woopsshitfuck
    forgot some of the custom weapons heh

--- gungaym/_extracustomcontent/gamemodes/gungaym/entities/weapons/weapon_python/shared.lua
+++ gungaym/_extracustomcontent/gamemodes/gungaym/entities/weapons/weapon_python/shared.lua
@@ -2,10 +2,7 @@ SWEP.Category				= "Cole3017"
 SWEP.PrintName 				= "Garter .44" 
 SWEP.Author		                	= "cole3017"
 SWEP.Purpose				= "Shooting and killing. At least i hope so."
-SWEP.MuzzleAttachment			= 3
-SWEP.ShellEjectAttachment			= 1
 SWEP.HoldType 				= "pistol"
-SWEP.ViewModelFlip	                = false
 SWEP.ViewModelFOV		= 80
 SWEP.WeaponDeploySpeed                  = 0.8
 SWEP.Base 				= "weapon_cs_base"
@@ -14,44 +11,39 @@ SWEP.AdminSpawnable 			= true
 SWEP.Slot = 1
 SWEP.SlotPos = 1
 SWEP.IconLetter			= "g"
-SWEP.FiresUnderwater = false
-SWEP.ViewModel 				= "models/weapons/v_pist_python.mdl"
-SWEP.WorldModel 				= "models/weapons/w_pist_deagle.mdl"
-SWEP.Primary.Cone		        = 0.01
+
 
 SWEP.Primary.Sound 			= "weapons/python/python-1.wav"
-SWEP.Primary.Recoil				= 7
-SWEP.Primary.Damage 			= 75
-SWEP.Primary.NumShots 			= 1
-SWEP.Primary.ClipSize 			= 6
-SWEP.Primary.Delay 			= 0.45
-SWEP.Primary.DefaultClip 			= 12
-SWEP.Primary.Automatic 			= false
-SWEP.Primary.Ammo 			= "smg1"
-SWEP.Primary.Tracer	= 0
-SWEP.Primary.Force      = 17
 SWEP.ViewModelFlip	= true
 SWEP.Class			= "weapon_python"
+SWEP.HoldType			= "pistol"
+SWEP.Base				= "weapon_cs_base"
+SWEP.Category			= "Counter-Strike"
+
+SWEP.Spawnable			= true
+SWEP.AdminSpawnable		= true
+
+SWEP.ViewModel 				= "models/weapons/v_pist_python.mdl"
+SWEP.WorldModel 				= "models/weapons/w_pist_deagle.mdl"
+
+SWEP.Weight				= 5
+SWEP.AutoSwitchTo		= false
+SWEP.AutoSwitchFrom		= false
+
+SWEP.Primary.Recoil			= 2
+SWEP.Primary.Damage			= 75
+SWEP.Primary.NumShots		= 1
+SWEP.Primary.Cone			= 0.005
+SWEP.Primary.ClipSize		= 8
+SWEP.Primary.Delay			= 0.3
+SWEP.Primary.DefaultClip	= 16
+SWEP.Primary.Automatic		= false
+SWEP.Primary.Ammo			= "smg1"
+SWEP.ZoomFOV				= 80
+SWEP.ZoomTime				= .1
+SWEP.HeadshotMult 			= 3
 
 
 SWEP.IronSightsPos = Vector(2.289, 0, 0.768)
 SWEP.IronSightsAng = Vector(0.843, 0.013, 0)
-
-
-
-
-
-
-function SWEP:Deploy()
-
-self.Weapon:EmitSound("hl2_base_sounds/draw.wav")
-	self.Weapon:SendWeaponAnim( ACT_VM_DRAW )
-	
-	self.Reloadaftershoot = CurTime() + 1
-	self.Weapon:SetNextPrimaryFire(CurTime() + 1)
-
-	return true
-end
-
-
-	
+SWEP.Spray				= 0
